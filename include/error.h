80cf21b1186bcd882cc114b166cd7e8361cbd828
milox_t <milox_t@epitech.eu>
2014-05-24 01:10:41 +0200

    dfsdfsd

--- include/error.h
+++ include/error.h
@@ -5,7 +5,7 @@
 ** Login   <chambo_e@epitech.net>
 **
 ** Started on  Tue May 20 22:05:19 2014 chambon emmanuel
-** Last update Fri May 23 22:00:30 2014 chambon emmanuel
+** Last update Sat May 24 01:09:13 2014 thomas milox
 */
 
 #ifndef __ERROR_H__
@@ -21,9 +21,9 @@
 # define ERR_ENV			"Error: 'ENV' is NULL.\n"
 # define ERR_FORK			"Error: with 'fork' syscall function.\n"
 # define ERR_MALLOC			"Error: something wrong occured with 'malloc syscall'.\n"
-# define ERR_CMD			"Shell: error-> [%s] command not found.\n"
-# define ERR_STR_TO_TAB			"Error: something went wrong with 'str_to_tab' call.\n"
-# define SEG_HANDLER			"Segmentation Fault !\n"
-# define ACCESS_DENIED			"access : [%s]\n"
+# define ERR_CMD			"Shell: error: [%s] command not found.\n"
+# define ERR_STR_TO_TAB			"Error: something went wrong with 'str_word_tab' call.\n"
+# define SEG_HANDLER			"Segmentation Fault .\n"
+# define ACCESS_DENIED			"access : error [%s]\n"
 
 #endif /* !__ERROR_H__ */
