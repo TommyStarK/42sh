4fcd38d58bf4d91f71e1887fbc96bef288adace7
chambo_e <chambo_e@epitech.eu>
2014-05-25 23:33:02 +0200

    very final push

--- include/error.h
+++ include/error.h
@@ -5,31 +5,32 @@
 ** Login   <chambo_e@epitech.net>
 **
 ** Started on  Tue May 20 22:05:19 2014 chambon emmanuel
-** Last update Sun May 25 23:09:05 2014 chambon emmanuel
+** Last update Sun May 25 23:28:53 2014 chambon emmanuel
 */
 
 #ifndef __ERROR_H__
 # define __ERROR_H__
 
-# define NO_CONF_FILE			"No configuration file found. Default value will be used\n"
-# define ARG_CD				"Too much argument for cd !\n"
-# define MISSING_ENV			"%.*s not found in env\n"
-# define ARG_SETENV			"Wrong argument for setenv !\n"
-# define PB_ENV				"A problem happened with env, please retry\n"
-# define ARG_UNSET			"Wrong argument for unsetenv !\n"
-# define NT_FND_ENV			"Item not found in env\n"
-# define ERR_ENV			"Error: 'ENV' is NULL.\n"
-# define ERR_FORK			"Error: with 'fork' syscall function.\n"
-# define ERR_MALLOC			"Error: something wrong occured with 'malloc syscall'.\n"
-# define ERR_CMD			"Shell: error: [%s] command not found.\n"
-# define SEG_HANDLER			"Segmentation Fault !\n"
-# define ACCESS_DENIED			"access : [%s]\n"
-# define WRONG_COLOR			"%s: color not found. Applying white instead.\n"
-# define INVALID_ALIAS			"Alias : invalid syntax.\n"
-# define INVALID_VALUE			"\"%s\" is not valid in this context\n"
-# define NO_ENV				"42sh: event not found: %d\n"
-# define ERROR_HISTORY			"Error with [.history] file\n"
-# define NO_TERMCAPS			"Your terminal doesn't handle all termcaps required for line editor\n"
-# define USAGE_SOURCE			"Usage : source path/to/conf/file\n"
+# define NO_CONF_FILE	"No configuration file found. Default value will be used\n"
+# define ARG_CD		"Too much argument for cd !\n"
+# define MISSING_ENV	"%.*s not found in env\n"
+# define ARG_SETENV	"Wrong argument for setenv !\n"
+# define PB_ENV		"A problem happened with env, please retry\n"
+# define ARG_UNSET	"Wrong argument for unsetenv !\n"
+# define NT_FND_ENV	"Item not found in env\n"
+# define ERR_ENV	"Error: 'ENV' is NULL.\n"
+# define ERR_FORK	"Error: with 'fork' syscall function.\n"
+# define ERR_MALLOC	"Something wrong occured with 'malloc syscall'.\n"
+# define ERR_CMD	"Shell: error: [%s] command not found.\n"
+# define SEG_HANDLER	"Segmentation Fault !\n"
+# define ACCESS_DENIED	"access : [%s]\n"
+# define WRONG_COLOR	"%s: color not found. Applying white instead.\n"
+# define INVALID_ALIAS	"Alias : invalid syntax.\n"
+# define INVALID_VALUE	"\"%s\" is not valid in this context\n"
+# define NO_ENV		"42sh: event not found: %d\n"
+# define ERROR_HISTORY	"Error with [.history] file\n"
+# define NO_TERMCAPS	"Your terminal doesn't handle all termcaps\n"
+# define USAGE_SOURCE	"Usage : source path/to/conf/file\n"
+# define URANDOM	"Cannot handle urandom\n"
 
 #endif /* !__ERROR_H__ */
--- src/node_shell.c
+++ src/node_shell.c
@@ -5,16 +5,35 @@
 ** Login   <chambo_e@epitech.net>
 **
 ** Started on  Tue May 20 22:17:13 2014 chambon emmanuel
-** Last update Sun May 25 21:37:05 2014 chambon emmanuel
+** Last update Sun May 25 23:27:09 2014 chambon emmanuel
 */
 
 #include "42.h"
+
+int		is_ascii(char *str)
+{
+  int		i;
+
+  i = 0;
+  while (str[i])
+    {
+      if ((unsigned char)str[i] > 254 || (unsigned char)str[i] < 1)
+	{
+	  fprintf(stderr, URANDOM);
+	  return (-1);
+	}
+      i++;
+    }
+  return (0);
+}
 
 int		node_shell(t_editor *editor)
 {
   char		*buffer;
 
   if (!(buffer = read_stdin(editor)))
+    return (-1);
+  if (is_ascii(buffer))
     return (-1);
   buffer = replace_alias(buffer, &editor->sh, 0, NULL);
   editor->sh.tree = create_binary_tree(&editor->sh.tree,
